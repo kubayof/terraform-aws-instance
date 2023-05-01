c0aca7d3e0b7749246d7ed117a3e1f0a70314d60
sam <oleh.kubai@nordstrom.com>
2023-05-01 13:43:44 +0300

    Update to use instance

--- main.tf
+++ main.tf
@@ -1,4 +1,27 @@
-resource "local_file" "index_html" {
-  filename = "index.html"
-  content = "<h1>Hello world!</h1>"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.64.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "dce"
+}
+
+resource "aws_instance" "cluster" {
+  tags = {
+    "Name": "test-provisioner"
+  }
+  ami = "ami-009c5f630e96948cb"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-07d3b96a2d1e16e3d"
+  key_name = "aws-eb"
+  user_data = <<EOT
+  #!/bin/bash
+
+  echo "Hello world" > /home/nonroot/index.html
+  EOT
 }
